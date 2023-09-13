54a184b6ce427e537ce4f87a5b74467074c9e754
CarlosIsra <2019452016@teschi.edu.mx>
2023-09-12 19:46:53 -0600

    2

--- APITESCHI/APITESCHI/urls.py
+++ APITESCHI/APITESCHI/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('',home.as_view(),name='index'),
+    
 ]
--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class home (APIView):
+    template_name="index.html"
+    def get (self, resques):
+        return render(resques, self.template_name)
